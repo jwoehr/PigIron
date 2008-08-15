ff613ad970e3b3b4e96054659e6977942d92bb9e
nobody <nobody@cc096084-c40e-4ef4-b8df-5c42cd912ce0>
2008-08-15 06:56:59 +0000

    This commit was manufactured by cvs2svn to create tag 'WORKS_AND_COMMENTED_20080815'.

---
+++ tags/WORKS_AND_COMMENTED_20080815/pigiron/src/com/softwoehr/pigiron/access/paramstructs/ImageNameArray.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2008, Jack J. Woehr jwoehr@softwoehr.com
+ * PO Box 51, Golden, Colorado 80402-0051 USA
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer
+ *         in the documentation and/or other materials provided with the
+ *         distribution.
+ *     * Neither the name of the PigIron Project nor the names of its
+ *         contributors may be used to endorse or promote products derived
+ *         from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.softwoehr.pigiron.access.paramstructs;
+
+import com.softwoehr.pigiron.access.VSMArray;
+
+/**
+ * A repeating array of <tt>ImageNameStruct</tt>
+ * instances.
+ * @see com.softwoehr.pigiron.access.paramstructs.ImageNameStruct
+ * @author jax
+ */
+public class ImageNameArray extends VSMArray {
+
+    /**
+     * Create a modelled-for-read instance with a specified formal name.
+     * @param formalName the formal name
+     * @return the modelled instance.
+     */
+    public static ImageNameArray modelArray(String formalName) {
+        ImageNameArray result = new ImageNameArray();
+        result.add(new ImageNameStruct(null, "image_name_struct"));
+        result.setFormalName(formalName);
+        return result;
+
+    }
+
+    /**
+     * Create an instance by copying the value from a like instance, and
+     * assign also the formal name.
+     * @param value a like instance to copy from
+     * @param formalName the formal name
+     */
+    public ImageNameArray(VSMArray value, String formalName) {
+        super(value, formalName);
+    }
+
+    /**
+     * Create an instance by copying the value from a like instance.
+     * @param value a like instance to copy from
+     */
+    public ImageNameArray(VSMArray value) {
+        super(value);
+    }
+
+    /**
+     * Create an instance of undefined value.
+     */
+    public ImageNameArray() {
+    }
+}
---
+++ tags/WORKS_AND_COMMENTED_20080815/pigiron/src/com/softwoehr/pigiron/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * PigIron is a Java class library for
+ * carrying out IBM &reg; z/VM &reg; V5R3.0
+ * Systems Management Application Programming
+ * transactions over TCP/IP sockets.
+ *
+ * <tt>com.softwoehr.pigiron</tt> is the base of the
+ * communications and function call portion
+ * of the effort.
+ *
+ * @see com.softwoehr.pigiron.access
+ */
+package com.softwoehr.pigiron;
