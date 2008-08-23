af5d2af9ff43c654fffc651e28adaec4fa84633a
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-08-23 03:41:35 +0000

    model for biz object development

---
+++ pigiron/src/com/softwoehr/pigiron/bizobj/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Provides the classes interface between
+ * the basic VSMAPI parameter types and basic
+ * VSMAPI function calls on the one hand and
+ * presentation layers on the other.
+ * 
+ * Subpackages provide the definitions
+ * of constraints and a shadow mechanism
+ * for usage of VSMAPI function calls with
+ * constraint checking applied.
+ *
+ * @see com.softwoehr.pigiron.access.paramstructs
+ * @see com.softwoehr.pigiron.functions
+ */
+package com.softwoehr.pigiron.bizobj;
