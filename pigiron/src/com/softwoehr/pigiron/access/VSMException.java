6bcd268c3e54214c21cfca86f4c0b8bdcfb6d8b0
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-08-23 03:40:26 +0000

    comments

--- pigiron/src/com/softwoehr/pigiron/access/VSMException.java
+++ pigiron/src/com/softwoehr/pigiron/access/VSMException.java
@@ -29,7 +29,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package com.softwoehr.pigiron.access;
 
 /**
@@ -61,9 +60,11 @@ public class VSMException extends Exception {
         super(message, cause);
     }
 
+    /**
+     * Create exception with message instanced.
+     * @param message a text message
+     */
     public VSMException(String message) {
         super(message);
     }
-
-
 }
