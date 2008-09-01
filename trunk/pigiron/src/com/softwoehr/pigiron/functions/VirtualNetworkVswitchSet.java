568e75c149a9474ae4dcf23b52a5c5f336b9338a
jwoehr <jwoehr@cc096084-c40e-4ef4-b8df-5c42cd912ce0>
2008-09-01 22:24:04 +0000

    Virtual_Network_Vswitch_Set

--- trunk/pigiron/src/com/softwoehr/pigiron/functions/VirtualNetworkVswitchSet.java
+++ trunk/pigiron/src/com/softwoehr/pigiron/functions/VirtualNetworkVswitchSet.java
@@ -39,18 +39,49 @@ package com.softwoehr.pigiron.functions;
 import java.io.IOException;
 import java.util.Iterator;
 import com.softwoehr.pigiron.access.*;
-import com.softwoehr.pigiron.access.paramstructs.VswitchArray;
 
 /**
- * {@code Virtual_Network_Vswitch_Delete} VSMAPI Function
- * @see com.softwoehr.pigiron.access.paramstructs.VswitchArray
+ * {@code Virtual_Network_Vswitch_Set} VSMAPI Function
  */
-public class VirtualNetworkVswitchDelete extends VSMCall {
+public class VirtualNetworkVswitchSet extends VSMCall {
 
     /**
      * The transmitted name of the function.
      */
-    public static final String FUNCTION_NAME = "Virtual_Network_Vswitch_Delete";
+    public static final String FUNCTION_NAME = "Virtual_Network_Vswitch_Set";
+
+    /** Unspecified */
+    public static final int CONNECTION_VALUE_UNSPECIFIED = 0;
+
+    /** Activate connection */
+    public static final int CONNECTION_VALUE_ACTIVATE = 1;
+
+    /** Dont activate connection' */
+    public static final int CONNECTION_VALUE_DO_NOT_ACTIVATE = 2;
+
+    /** Unspecified */
+    public static final int ROUTING_VALUE_UNSPECIFIED = 0;
+
+    /** Device identified in real_device_address will not act as a router to the virtual switch */
+    public static final int ROUTING_VALUE_NONROUTER = 1;
+
+    /** device identified in real_device_address will act as a primary router to the virtual switch */
+    public static final int ROUTING_VALUE_PRIROUTER = 2;
+
+    /** Unspecified */
+    public static final int TRANSPORT_TYPE_UNSPECIFIED = 0;
+
+    /** IP */
+    public static final int TRANSPORT_TYPE_IP = 1;
+
+    /** Ethernet */
+    public static final int TRANSPORT_TYPE_ETHERNET = 2;
+
+    /** Unspecified */
+    public static final int PORT_TYPE_UNSPECIFIED = 0;
+
+    /** Access */
+    public static final int PORT_TYPE_ACCESS = 1;
 
     /** Trunk */
     public static final int PORT_TYPE_TRUNK = 2;
@@ -79,7 +110,7 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
     /**
      *  Create an instance of the function call with important fields not instanced.
      */
-    public VirtualNetworkVswitchDelete() {
+    public VirtualNetworkVswitchSet() {
     }
 
     /**
@@ -90,17 +121,8 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
      * @param password the password
      * @param target_identifier the target of the VSMAPI function
      * @param switch_name instances {@code switchName}
-     * @param update_system_config_indicator instances {@code updateSystemConfigIndicator}
-     * @param system_config_name instances {@code systemConfigName}
-     * @param system_config_type instances {@code systemConfigType}
-     * @param parm_disk_number instances {@code parmDiskNumber}
-     * @param parm_disk_owner instances {@code parmDiskOwner}
-     * @param parm_disk_password instances {@code parmDiskPassword}
-     * @param alt_system_config_type instances {@code altSystemConfigType}
-     * @param alt_parm_disk_owner instances {@code altParmDiskOwner}
-     * @param alt_parm_disk_number instances {@code altParmDiskNumber}
      */
-    public VirtualNetworkVswitchDelete(String hostname, int port, String userid, String password, String target_identifier, String switch_name, int update_system_config_indicator, String system_config_name, String system_config_type, String parm_disk_number, String parm_disk_owner, String parm_disk_password, String alt_system_config_type, String alt_parm_disk_owner, String alt_parm_disk_number) {
+    public VirtualNetworkVswitchSet(String hostname, int port, String userid, String password, String target_identifier, String switch_name) {
         this();
         setHostname(hostname);
         setPort(port);
@@ -108,19 +130,40 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
         setPassword(password);
         setTarget_identifier(target_identifier);
         set_switchName(switch_name);
-        set_updateSystemConfigIndicator(update_system_config_indicator);
-        set_systemConfigName(system_config_name);
-        set_systemConfigType(system_config_type);
-        set_parmDiskNumber(parm_disk_number);
-        set_parmDiskOwner(parm_disk_owner);
-        set_parmDiskPassword(parm_disk_password);
-        set_altSystemConfigType(alt_system_config_type);
-        set_altParmDiskOwner(alt_parm_disk_owner);
-        set_altParmDiskNumber(alt_parm_disk_number);
     }
 
     /** The name of the virtual switch */
     private String switchName = "";
+
+    /** A userid to be added to the access list for the specified virtual switch */
+    private String grantUserid = "";
+
+    /** The user VLAN ID */
+    private String userVlanId = "";
+
+    /** A userid to be removed from the access list for the specified virtual switch */
+    private String revokeUserid = "";
+
+    /** The real device address of a real OSA-Express QDIO device */
+    private String realDeviceAddress = "";
+
+    /** The name used to identify the OSA Expanded adapter */
+    private String portName = "";
+
+    /** The userid controlling the real device or * for any available */
+    private String controllerName = "*";
+
+    /** Activate/do not */
+    private int connectionValue = 0;
+
+    /** Specifies the QDIO buffer size in megabytes. */
+    private int queueMemoryLimit = 0;
+
+    /** Specifies whether the OSA-Express QDIO device will act as a router to the virtual switch */
+    private int routingValue = 0;
+
+    /** Specifies the port type */
+    private int portType = -1;
 
     /** Configuration options */
     private int updateSystemConfigIndicator = -1;
@@ -155,6 +198,9 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
     /** Multiwrite password for the second (alternative) parm disk */
     private String altParmDiskPassword = ",";
 
+    /** GVRP value */
+    private int gvrpValue = GVRP_VALUE_UNSPECIFIED;
+
     /** Set the value of {@code  switchName }.
      * @param val The value to set {@code  switchName }.
      */
@@ -167,6 +213,146 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
      */
     public String get_switchName() {
         return switchName;
+    }
+
+    /** Set the value of {@code  grantUserid }.
+     * @param val The value to set {@code  grantUserid }.
+     */
+    public void set_grantUserid(String val) {
+        grantUserid = val;
+    }
+
+    /** Get the value of {@code  grantUserid }.
+     * @return The value of {@code  grantUserid }.
+     */
+    public String get_grantUserid() {
+        return grantUserid;
+    }
+
+    /** Set the value of {@code  userVlanId }.
+     * @param val The value to set {@code  userVlanId }.
+     */
+    public void set_userVlanId(String val) {
+        userVlanId = val;
+    }
+
+    /** Get the value of {@code  userVlanId }.
+     * @return The value of {@code  userVlanId }.
+     */
+    public String get_userVlanId() {
+        return userVlanId;
+    }
+
+    /** Set the value of {@code  revokeUserid }.
+     * @param val The value to set {@code  revokeUserid }.
+     */
+    public void set_revokeUserid(String val) {
+        revokeUserid = val;
+    }
+
+    /** Get the value of {@code  revokeUserid }.
+     * @return The value of {@code  revokeUserid }.
+     */
+    public String get_revokeUserid() {
+        return revokeUserid;
+    }
+
+    /** Set the value of {@code  realDeviceAddress }.
+     * @param val The value to set {@code  realDeviceAddress }.
+     */
+    public void set_realDeviceAddress(String val) {
+        realDeviceAddress = val;
+    }
+
+    /** Get the value of {@code  realDeviceAddress }.
+     * @return The value of {@code  realDeviceAddress }.
+     */
+    public String get_realDeviceAddress() {
+        return realDeviceAddress;
+    }
+
+    /** Set the value of {@code  portName }.
+     * @param val The value to set {@code  portName }.
+     */
+    public void set_portName(String val) {
+        portName = val;
+    }
+
+    /** Get the value of {@code  portName }.
+     * @return The value of {@code  portName }.
+     */
+    public String get_portName() {
+        return portName;
+    }
+
+    /** Set the value of {@code  controllerName }.
+     * @param val The value to set {@code  controllerName }.
+     */
+    public void set_controllerName(String val) {
+        controllerName = val;
+    }
+
+    /** Get the value of {@code  controllerName }.
+     * @return The value of {@code  controllerName }.
+     */
+    public String get_controllerName() {
+        return controllerName;
+    }
+
+    /** Set the value of {@code  connectionValue }.
+     * @param val The value to set {@code  connectionValue }.
+     */
+    public void set_connectionValue(int val) {
+        connectionValue = val;
+    }
+
+    /** Get the value of {@code  connectionValue }.
+     * @return The value of {@code  connectionValue }.
+     */
+    public int get_connectionValue() {
+        return connectionValue;
+    }
+
+    /** Set the value of {@code  queueMemoryLimit }.
+     * @param val The value to set {@code  queueMemoryLimit }.
+     */
+    public void set_queueMemoryLimit(int val) {
+        queueMemoryLimit = val;
+    }
+
+    /** Get the value of {@code  queueMemoryLimit }.
+     * @return The value of {@code  queueMemoryLimit }.
+     */
+    public int get_queueMemoryLimit() {
+        return queueMemoryLimit;
+    }
+
+    /** Set the value of {@code  routingValue }.
+     * @param val The value to set {@code  routingValue }.
+     */
+    public void set_routingValue(int val) {
+        routingValue = val;
+    }
+
+    /** Get the value of {@code  routingValue }.
+     * @return The value of {@code  routingValue }.
+     */
+    public int get_routingValue() {
+        return routingValue;
+    }
+
+    /** Set the value of {@code  portType }.
+     * @param val The value to set {@code  portType }.
+     */
+    public void set_portType(int val) {
+        portType = val;
+    }
+
+    /** Get the value of {@code  portType }.
+     * @return The value of {@code  portType }.
+     */
+    public int get_portType() {
+        return portType;
     }
 
     /** Set the value of {@code  updateSystemConfigIndicator }.
@@ -323,6 +509,20 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
         return altParmDiskPassword;
     }
 
+    /** Set the value of {@code  gvrpValue }.
+     * @param val The value to set {@code  gvrpValue }.
+     */
+    public void set_gvrpValue(int val) {
+        gvrpValue = val;
+    }
+
+    /** Get the value of {@code  gvrpValue }.
+     * @return The value of {@code  gvrpValue }.
+     */
+    public int get_gvrpValue() {
+        return gvrpValue;
+    }
+
     /**
      * Marshall parameters for the VSMAPI function call.
      * "Input" as in "input to VSMAPI".
@@ -348,6 +548,28 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
         tempString = new VSMString(get_switchName(), "switch_name");
         parameterArray.add(new VSMInt4(tempString.paramLength(), "switch_name_length"));
         parameterArray.add(tempString);
+        tempString = new VSMString(get_grantUserid(), "grant_userid");
+        parameterArray.add(new VSMInt4(tempString.paramLength(), "grant_userid_length"));
+        parameterArray.add(tempString);
+        tempString = new VSMString(get_userVlanId(), "user_vlan_id");
+        parameterArray.add(new VSMInt4(tempString.paramLength(), "user_vlan_id_length"));
+        parameterArray.add(tempString);
+        tempString = new VSMString(get_revokeUserid(), "revoke_userid");
+        parameterArray.add(new VSMInt4(tempString.paramLength(), "revoke_userid_length"));
+        parameterArray.add(tempString);
+        tempString = new VSMString(get_realDeviceAddress(), "real_device_address");
+        parameterArray.add(new VSMInt4(tempString.paramLength(), "real_device_address_length"));
+        parameterArray.add(tempString);
+        tempString = new VSMString(get_portName(), "port_name");
+        parameterArray.add(new VSMInt4(tempString.paramLength(), "port_name_length"));
+        parameterArray.add(tempString);
+        tempString = new VSMString(get_controllerName(), "controller_name");
+        parameterArray.add(new VSMInt4(tempString.paramLength(), "controller_name_length"));
+        parameterArray.add(tempString);
+        parameterArray.add(new VSMInt1(get_connectionValue(), "connection_value"));
+        parameterArray.add(new VSMInt4(get_queueMemoryLimit(), "queue_memory_limit"));
+        parameterArray.add(new VSMInt1(get_routingValue(), "routing_value"));
+        parameterArray.add(new VSMInt1(get_portType(), "port_type"));
         parameterArray.add(new VSMInt1(get_updateSystemConfigIndicator(), "update_system_config_indicator"));
         tempString = new VSMString(get_systemConfigName(), "system_config_name");
         parameterArray.add(new VSMInt4(tempString.paramLength(), "system_config_name_length"));
@@ -379,6 +601,7 @@ public class VirtualNetworkVswitchDelete extends VSMCall {
         tempString = new VSMString(get_altParmDiskPassword(), "alt_parm_disk_password");
         parameterArray.add(new VSMInt4(tempString.paramLength(), "alt_parm_disk_password_length"));
         parameterArray.add(tempString);
+        parameterArray.add(new VSMInt1(get_gvrpValue(), "gvrp_value"));
         VSMInt4 outputLength = new VSMInt4(new Long(parameterArray.totalParameterLength()).intValue(), "output_length");
         parameterArray.insertElementAt(outputLength, 0);
         setInParams(parameterArray);
