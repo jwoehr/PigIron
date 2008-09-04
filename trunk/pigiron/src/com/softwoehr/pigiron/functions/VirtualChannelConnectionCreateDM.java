7609a9ec5fcc8add12c133e078908624f47596d0
jwoehr <jwoehr@cc096084-c40e-4ef4-b8df-5c42cd912ce0>
2008-09-04 06:52:53 +0000

    Virtual_Channel_Connection_Create_DM

--- trunk/pigiron/src/com/softwoehr/pigiron/functions/VirtualChannelConnectionCreateDM.java
+++ trunk/pigiron/src/com/softwoehr/pigiron/functions/VirtualChannelConnectionCreateDM.java
@@ -41,19 +41,19 @@ import java.util.Iterator;
 import com.softwoehr.pigiron.access.*;
 
 /**
- * <tt>Virtual_Channel_Connection_Create</tt> VSMAPI Function
+ * <tt>Virtual_Channel_Connection_Create_DM</tt> VSMAPI Function
  */
-public class VirtualChannelConnectionCreate extends VSMCall {
+public class VirtualChannelConnectionCreateDM extends VSMCall {
 
     /**
      * The transmitted name of the function.
      */
-    public static final String FUNCTION_NAME = "Virtual_Channel_Connection_Create";
+    public static final String FUNCTION_NAME = "Virtual_Channel_Connection_Create_DM";
 
     /**
      *  Create an instance of the function call with important fields not instanced.
      */
-    public VirtualChannelConnectionCreate() {
+    public VirtualChannelConnectionCreateDM() {
     }
 
     /**
@@ -65,9 +65,8 @@ public class VirtualChannelConnectionCreate extends VSMCall {
      * @param target_identifier the target of the VSMAPI function
      * @param image_device_number instances {@code imageDeviceNumber}
      * @param coupled_image_name instances {@code coupledImageName}
-     * @param coupled_image_device_number instances {@code coupledImageDeviceNumber}
      */
-    public VirtualChannelConnectionCreate(String hostname, int port, String userid, String password, String target_identifier, String image_device_number, String coupled_image_name, String coupled_image_device_number) {
+    public VirtualChannelConnectionCreateDM(String hostname, int port, String userid, String password, String target_identifier, String image_device_number, String coupled_image_name) {
         this();
         setHostname(hostname);
         setPort(port);
@@ -76,7 +75,6 @@ public class VirtualChannelConnectionCreate extends VSMCall {
         setTarget_identifier(target_identifier);
         set_imageDeviceNumber(image_device_number);
         set_coupledImageName(coupled_image_name);
-        set_coupledImageDeviceNumber(coupled_image_device_number);
     }
 
     /** The virtual device number of the network device in the active virtual image */
@@ -84,9 +82,6 @@ public class VirtualChannelConnectionCreate extends VSMCall {
 
     /** The virtual image name of the target virtual image that is to be connected */
     private String coupledImageName = "";
-
-    /** The virtual device number of the network device in another virtual image */
-    private String coupledImageDeviceNumber = "";
 
     /** Set the value of {@code  imageDeviceNumber }.
      * @param val The value to set {@code  imageDeviceNumber }.
@@ -116,20 +111,6 @@ public class VirtualChannelConnectionCreate extends VSMCall {
         return coupledImageName;
     }
 
-    /** Set the value of {@code  coupledImageDeviceNumber }.
-     * @param val The value to set {@code  coupledImageDeviceNumber }.
-     */
-    public void set_coupledImageDeviceNumber(String val) {
-        coupledImageDeviceNumber = val;
-    }
-
-    /** Get the value of {@code  coupledImageDeviceNumber }.
-     * @return The value of {@code  coupledImageDeviceNumber }.
-     */
-    public String get_coupledImageDeviceNumber() {
-        return coupledImageDeviceNumber;
-    }
-
     /**
      * Marshall parameters for the VSMAPI function call.
      * "Input" as in "input to VSMAPI".
@@ -157,9 +138,6 @@ public class VirtualChannelConnectionCreate extends VSMCall {
         parameterArray.add(tempString);
         tempString = new VSMString(get_coupledImageName(), "coupled_image_name");
         parameterArray.add(new VSMInt4(tempString.paramLength(), "coupled_image_name_length"));
-        parameterArray.add(tempString);
-        tempString = new VSMString(get_coupledImageDeviceNumber(), "coupled_image_device_number");
-        parameterArray.add(new VSMInt4(tempString.paramLength(), "coupled_image_device_number_length"));
         parameterArray.add(tempString);
         VSMInt4 outputLength = new VSMInt4(new Long(parameterArray.totalParameterLength()).intValue(), "output_length");
         parameterArray.insertElementAt(outputLength, 0);
@@ -203,15 +181,15 @@ public class VirtualChannelConnectionCreate extends VSMCall {
      */
     public static void main(String[] argv) throws IOException, VSMException {
 
-        VirtualChannelConnectionCreate instance = null;
+        VirtualChannelConnectionCreateDM instance = null;
 
-        if (argv.length != 8) {
-            System.out.println("usage: args are:\ninetaddr port user pw target coupled_image_name coupled_image_device_number");
+        if (argv.length != 7) {
+            System.out.println("usage: args are:\ninetaddr port user pw target image_device_number coupled_image_name");
             System.exit(1);
         }
 
-        System.out.println("Args are: " + argv[0] + " " + argv[1] + " " + argv[2] + " " + argv[3] + " " + argv[4] + " " + argv[5] + " " + argv[6] + " " + argv[7]);
-        instance = new VirtualChannelConnectionCreate(argv[0], Integer.valueOf(argv[1]).intValue(), argv[2], argv[3], argv[4],  argv[5], argv[6], argv[7]);
+        System.out.println("Args are: " + argv[0] + " " + argv[1] + " " + argv[2] + " " + argv[3] + " " + argv[4] + " " + argv[5] + " " + argv[6]);
+        instance = new VirtualChannelConnectionCreateDM(argv[0], Integer.valueOf(argv[1]).intValue(), argv[2], argv[3], argv[4],  argv[5], argv[6]);
 
         ParameterArray pA = instance.doIt();
         System.out.println("Returns from call to " + instance.getFunctionName() + ":");
