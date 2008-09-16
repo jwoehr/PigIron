022637c2f6432d9df710ac23ce0c56e8b0752315
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-09-16 05:52:05 +0000

    working on webobj

--- pigiron/src/com/softwoehr/pigiron/webobj/topview/DirectoryEntry.java
+++ pigiron/src/com/softwoehr/pigiron/webobj/topview/DirectoryEntry.java
@@ -4,62 +4,32 @@
  */
 package com.softwoehr.pigiron.webobj.topview;
 
-import com.softwoehr.pigiron.webobj.JSONMarshaller;
-import com.softwoehr.pigiron.webobj.MarshallableObject;
-import com.softwoehr.pigiron.webobj.Marshaller;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * 
  * @author jax
  */
-public class DirectoryEntry extends MarshallableObject {
+public class DirectoryEntry extends JSONObject {
 
-    public String name;
-    public URI uriUri;
-    public String uri;
-
-    @Override
-    public String[] names() {
-        return new String[]{"name", "uri"};
+    public String getName() throws JSONException {
+        return getString("name");
     }
 
-    public DirectoryEntry(String name, URI uri) {
-        this.name = name;
-        this.uriUri = uri;
+    public URI getURI() throws URISyntaxException, JSONException {
+        return new URI(getString("uri"));
     }
 
-    public DirectoryEntry unpack_members(Marshaller marshaller) {
-        try {
-            uriUri = new URI(uri);
-        } catch (URISyntaxException ex) {
-            Logger.getLogger(DirectoryEntry.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return this;
+    public DirectoryEntry(String name, URI uri) throws JSONException {
+        put("name", name);
+        put("uri", uri);
     }
 
-    public DirectoryEntry prepare_members() {
-        uri = uriUri.toString();
-        return this;
-    }
-
-    @Override
-    public void fromRepresentation(String representation, Marshaller marshaller) {
-        super.fromRepresentation(representation, marshaller);
-        unpack_members(marshaller);
-    }
-
-    /*@Override
-    public void fromRepresentation(String representation, Marshaller marshaller, MarshallingTraits marshallingTraits) {
-    super.fromRepresentation(representation, marshaller, marshallingTraits);
-    }*/
-    @Override
-    public String toRepresentation(Marshaller marshaller) {
-        prepare_members();
-        return super.toRepresentation(marshaller);
+    public DirectoryEntry(String source) throws JSONException {
+        super(source);
     }
 
     /**
@@ -67,20 +37,11 @@ public class DirectoryEntry extends MarshallableObject {
      * @param argv
      * @throws URISyntaxException
      */
-    public static void main(String[] argv) throws URISyntaxException {
+    public static void main(String[] argv) throws URISyntaxException, JSONException {
         String name = argv[0];
         String uri = argv[1];
         String rerepresentation = argv[2];
         DirectoryEntry direntry = new DirectoryEntry(name, new URI(uri));
-        JSONMarshaller marshaller = new JSONMarshaller();
         System.out.println("The starting DirectorEntry: " + direntry);
-        System.out.println("   name: " + direntry.name);
-        System.out.println("   uriUri:  " + direntry.uriUri);
-        String representation = direntry.toRepresentation(marshaller);
-        System.out.println("Here's a fresh representation: " + representation);
-        direntry.fromRepresentation(rerepresentation, marshaller);
-        System.out.println("The rerepresented DirectorEntry: " + direntry);
-        System.out.println("   name:   " + direntry.name);
-        System.out.println("   uriUri: " + direntry.uriUri);
     }
 }
