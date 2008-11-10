16f98e12817eea17111fdb4f44c40bc404ef1262
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-11-10 23:09:23 +0000

    cleanup

--- pigiron/src/com/softwoehr/oobject/Oob.java
+++ pigiron/src/com/softwoehr/oobject/Oob.java
@@ -1,29 +1,61 @@
 package com.softwoehr.oobject;
 
-public class Oob
-{
-    public Object key;
-    public Object value;
-    
-    public Oob(Object key, Object value) {
-	this.key=key;
-	this.value=value;
-    }
-    
-    public String toString () {
-	StringBuffer sb = new StringBuffer(super.toString());
-	sb.append(" Oob.key: " + key + " Oob.value: " + value);
-	return sb.toString();
-    }
-    
-    public boolean equals (Oob oob) {
-	boolean result = (oob.key.equals(key) & oob.value.equals(value));
-	return result;
-    }
-    
-    public static void main(String[] args)
-    {    
-        
-    }
+/**
+ *  Description of the Class
+ *
+ * @author     jax
+ * @created    November 10, 2008
+ */
+public class Oob {
+	/**
+	 *  Description of the Field
+	 */
+	public Object key;
+	/**
+	 *  Description of the Field
+	 */
+	public Object value;
+
+	/**
+	 *Constructor for the Oob object
+	 *
+	 * @param  key    Description of the Parameter
+	 * @param  value  Description of the Parameter
+	 */
+	public Oob(Object key, Object value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @return    Description of the Return Value
+	 */
+	public String toString() {
+		StringBuffer sb = new StringBuffer(super.toString());
+		sb.append(" Oob.key: " + key + " Oob.value: " + value);
+		return sb.toString();
+	}
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  oob  Description of the Parameter
+	 * @return      Description of the Return Value
+	 */
+	public boolean equals(Oob oob) {
+		boolean result = (oob.key.equals(key) & oob.value.equals(value));
+		return result;
+	}
+
+	/**
+	 *  The main program for the Oob class
+	 *
+	 * @param  args  The command line arguments
+	 */
+	public static void main(String[] args) {
+
+	}
 }
 
--- pigiron/src/com/softwoehr/oobject/Oobary.java
+++ pigiron/src/com/softwoehr/oobject/Oobary.java
@@ -3,50 +3,82 @@ package com.softwoehr.oobject;
 import java.util.Hashtable;
 import java.util.Enumeration;
 
-public class Oobary
-{
-    private Hashtable oobs = new Hashtable();
-    
-    public Oobary() {
-    }
-    
-    public void put(Oob oob) {
-	oobs.put(oob.key, oob.value);
-    }
-    
-    public Oob get(Object key) {
-	Oob result = null;
-	if (oobs.containsKey(key)) {
-	    result = new Oob(key, oobs.get(key));
+/**
+ *  Description of the Class
+ *
+ * @author     jax
+ * @created    November 10, 2008
+ */
+public class Oobary {
+	private Hashtable oobs = new Hashtable();
+
+	/**
+	 *Constructor for the Oobary object
+	 */
+	public Oobary() { }
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  oob  Description of the Parameter
+	 */
+	public void put(Oob oob) {
+		oobs.put(oob.key, oob.value);
 	}
-	return result;
-    }
-    
-    public Oobary oobary(Class aclass) {
-	Oobary result = new Oobary ();
-	Enumeration e = oobs.keys();
-	while (e.hasMoreElements()) {
-	    Object k = e.nextElement();
-	    if (k.getClass() == aclass) {
-		result.put(get(k));
-	    }
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  key  Description of the Parameter
+	 * @return      Description of the Return Value
+	 */
+	public Oob get(Object key) {
+		Oob result = null;
+		if (oobs.containsKey(key)) {
+			result = new Oob(key, oobs.get(key));
+		}
+		return result;
 	}
-	return result;
-    }
-    
-    public String toString () {
-	StringBuffer sb = new StringBuffer(super.toString());
-	Enumeration e = oobs.keys();
-	while (e.hasMoreElements()) {
-	    Object k = e.nextElement();
-	    sb.append(" an Oob: " + get(k));
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  aclass  Description of the Parameter
+	 * @return         Description of the Return Value
+	 */
+	public Oobary oobary(Class aclass) {
+		Oobary result = new Oobary();
+		Enumeration e = oobs.keys();
+		while (e.hasMoreElements()) {
+			Object k = e.nextElement();
+			if (k.getClass() == aclass) {
+				result.put(get(k));
+			}
+		}
+		return result;
 	}
-	return sb.toString();
-    }
-    
-    public static void main(String[] args)
-    {    
-        
-    }
+
+	/**
+	 *  Description of the Method
+	 *
+	 * @return    Description of the Return Value
+	 */
+	public String toString() {
+		StringBuffer sb = new StringBuffer(super.toString());
+		Enumeration e = oobs.keys();
+		while (e.hasMoreElements()) {
+			Object k = e.nextElement();
+			sb.append(" an Oob: " + get(k));
+		}
+		return sb.toString();
+	}
+
+	/**
+	 *  The main program for the Oobary class
+	 *
+	 * @param  args  The command line arguments
+	 */
+	public static void main(String[] args) {
+	}
 }
 
