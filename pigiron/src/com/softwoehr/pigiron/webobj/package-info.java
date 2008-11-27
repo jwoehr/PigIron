0da4f9ffcc74f9bc8f6b63231ca0cd2458ba47d1
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-11-27 22:33:02 +0000

    Added the engine to execute JSON requests Also some docs

--- pigiron/src/com/softwoehr/pigiron/webobj/package-info.java
+++ pigiron/src/com/softwoehr/pigiron/webobj/package-info.java
@@ -1,17 +1,12 @@
 /**
- * PigIron is a Java class library for
- * carrying out IBM &reg; z/VM &reg; V5R3.0
- * Systems Management Application Programming
- * transactions over TCP/IP sockets.
- *
  * <tt>com.softwoehr.pigiron.webobj</tt> is where web object representations
  * of entities within the PigIron problem domain are found.
  *
  * The top of the <tt>com.softwoehr.pigiron.webobj</tt> package contains
- * outerface objects marshalled over xmlrpc and the like protocols and
- * practices.
+ * the outerface Engine which takes objects marshalled in JSON (and XML?)
+ * and performs PigIron VSMAPI functions in response.
  *
- * In the deeper packages are the deeper faces. The package layout can
+ * In the deeper packages are particular faces. The package layout can
  * become part of the URI scheme.
  *
  * @see com.softwoehr.pigiron
