1b145d5d642c8b8f6a9d32ae0f82d85a4f64b376
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-12-02 05:38:23 +0000

    typo caused by search/replace

--- pigiron/src/com/softwoehr/pigiron/access/ParameterArray.java
+++ pigiron/src/com/softwoehr/pigiron/access/ParameterArray.java
@@ -8,9 +8,9 @@
  * are met:
  *
  *     * Redistributions of source code must retain the above copyright
- *         notice, this currentListIterator of conditions and the following disclaimer.
+ *         notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
- *         notice, this currentListIterator of conditions and the following disclaimer
+ *         notice, this list of conditions and the following disclaimer
  *         in the documentation and/or other materials provided with the
  *         distribution.
  *     * Neither the name of the PigIron Project nor the names of its
