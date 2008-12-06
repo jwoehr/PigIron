4f1465770eeb738378d49488958a234dc80d4930
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2008-12-06 08:24:41 +0000

    comment

--- pigiron/src/com/softwoehr/pigiron/webobj/topview/functions/package-info.java
+++ pigiron/src/com/softwoehr/pigiron/webobj/topview/functions/package-info.java
@@ -1,5 +1,7 @@
 /**
  * <tt>com.softwoehr.pigiron.webobj.topview.functions</tt> is where web object
  * proxies for the individual PigIron VSMAPI functions are found.
+ * These functions do not all completely work in that the ones with object
+ * arguments do not create correctly instanced arguments yet, working on this.
  */
 package com.softwoehr.pigiron.webobj.topview.functions;
