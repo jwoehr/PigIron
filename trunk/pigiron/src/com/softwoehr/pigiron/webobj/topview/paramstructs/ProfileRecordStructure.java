a37074c43f77c4b5b18baf58d382740f31dd06e6
jwoehr <jwoehr@cc096084-c40e-4ef4-b8df-5c42cd912ce0>
2008-12-15 23:32:52 +0000

    working on instancing params from JSON

--- trunk/pigiron/src/com/softwoehr/pigiron/webobj/topview/paramstructs/ProfileRecordStructure.java
+++ trunk/pigiron/src/com/softwoehr/pigiron/webobj/topview/paramstructs/ProfileRecordStructure.java
@@ -32,32 +32,33 @@
 package com.softwoehr.pigiron.webobj.topview.paramstructs;
 
 import com.softwoehr.pigiron.access.VSMParm;
+import com.softwoehr.pigiron.access.VSMString;
 import com.softwoehr.pigiron.webobj.topview.*;
-
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.annotation.Generated;
+
 /**
  *  Description of the Class
  *
  * @author     jax
  * @created    December 6, 2008
  */
-public class ProfileRecordArray extends ParamProxy {
+ @Generated(value = {"ProfileRecordStructure"})
+public class ProfileRecordStructure extends ParamProxy {
     /**
      *  Description of the Method
      *
-     * @param  arg  Description of the Parameter
-     * @return      Description of the Return Value
+     * @param  arg                Argument whose value represent the ProfileRecordStructure
+     * @return                    Instanced 
+     * @exception  JSONException  on JSON err
      */ 
     public VSMParm from(Argument arg) throws JSONException {
-      	String formal_name = arg.getFormalName();
-        com.softwoehr.pigiron.access.paramstructs.ProfileRecordArray parm
-	= new com.softwoehr.pigiron.access.paramstructs.ProfileRecordArray(formal_name);
-        JSONArray jo = arg.getJSONArrayValue();
-	for (int i = 0; i < jo.length(); i++) {
-	   Argument a = new Argument(jo.opt(i).toString());
-	   parm.add((new ProfileRecordStructure()).from(a));
-	}
+	String formal_name = arg.getFormalName();
+        com.softwoehr.pigiron.access.paramstructs.ProfileRecordStructure parm
+	= new com.softwoehr.pigiron.access.paramstructs.ProfileRecordStructure(new VSMString(arg.getStringValue()), arg.getFormalName());
         return parm;
     }
 }
