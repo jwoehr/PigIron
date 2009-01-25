a38a7a07f687b812226cadd69e62f480eef9e145
nobody <nobody@cc096084-c40e-4ef4-b8df-5c42cd912ce0>
2009-01-25 09:40:32 +0000

    This commit was manufactured by cvs2svn to create tag 'START_PIGVIEW'.

--- pigiron/piggen/javaname.m4
+++
@@ -1,3 +0,0 @@
-divert(-1)dnl
-include(`pigsty.m4')dnl
-divert(0)dnl
--- pigiron/src/com/softwoehr/pigiron/webobj/topview/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * <tt>com.softwoehr.pigiron.webobj.topview</tt> is where web object representations
- * necessary to describing a request-and-response PigIron VSMAPI function are found.
- */
-package com.softwoehr.pigiron.webobj.topview;
