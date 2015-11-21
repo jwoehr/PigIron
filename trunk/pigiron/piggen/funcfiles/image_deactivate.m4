0a9672a1d444c6662cb9ba5fe280864d94c151cb
jwoehr <jwoehr@cc096084-c40e-4ef4-b8df-5c42cd912ce0>
2015-11-21 22:44:26 +0000

    changed ParameterArray

--- trunk/pigiron/piggen/funcfiles/image_deactivate.m4
+++ trunk/pigiron/piggen/funcfiles/image_deactivate.m4
@@ -2,7 +2,6 @@ include(`pigfunc.m4')dnl \\ image_deactivate.m4
 function_namespace(`Image_Deactivate',`failing_array')dnl
 pigfunc_start()dnl
 pigfunc_import(`java.io.IOException')dnl
-pigfunc_import(`java.util.Iterator')dnl
 pigfunc_import(`com.softwoehr.pigiron.access.*')dnl
 pigfunc_import(`com.softwoehr.pigiron.access.paramstructs.significant_parameter_classname')dnl
 pigfunc_class(function_classname,`VSMCall',`com.softwoehr.pigiron.functions',function_formal_name,`dnl
@@ -28,7 +27,7 @@ pigfunc_function(`public', `static', `void', `', `main', `String[] argv', `IOExc
      * @throws IOException on comm error
      * @throws VSMException on internal Pigiron param marshalling error', `dnl
 
-        function_classname instance = null;
+        function_classname instance;
 
         if (argv.length != 6) {
             System.out.println("usage: args are:\ninetaddr port user pw target_image force_time");
@@ -36,7 +35,7 @@ pigfunc_function(`public', `static', `void', `', `main', `String[] argv', `IOExc
         }
 
         System.out.println("Args are: " + argv[0] + " " + argv[1] + " " + argv[2] + " " + argv[3] + " " + argv[4] + " " + argv[5]);
-        instance = new function_classname()(argv[0], Integer.valueOf(argv[1]).intValue(), argv[2], argv[3], argv[4], argv[5]);
+        instance = new function_classname()(argv[0], Integer.valueOf(argv[1]), argv[2], argv[3], argv[4], argv[5]);
 
         ParameterArray pA = instance.doIt();
         System.out.println("Returns from call to " + instance.getFunctionName() + ":");
--- trunk/pigiron/src/com/softwoehr/pigiron/access/ParameterArray.java
+++ trunk/pigiron/src/com/softwoehr/pigiron/access/ParameterArray.java
@@ -36,49 +36,49 @@ import com.softwoehr.pigiron.functions.VSMCall;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Enumeration;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
-import java.util.Vector;
 
 /**
  * Parameter composition, marshalling and transmission nexus.
+ *
  * @author jax
  */
-public class ParameterArray extends Vector <VSMParm> {
+public class ParameterArray extends ArrayList<VSMParm> {
 
     /**
      * Byte size of <tt>int1</tt> datatype
-     */ 
+     */
     public static final int SIZEOF_INT1 = 1;
     /**
      * Byte size of <tt>int4</tt> datatype
-     */ 
+     */
     public static final int SIZEOF_INT4 = 4;
     /**
      * Byte size of <tt>int8</tt> datatype
-     */ 
+     */
     public static final int SIZEOF_INT8 = 8;
 
-    /** 
-     * The VSMCall function using this ParameterArray.
-     * ParameterArray needs this for interpreting
-     * return/reason code pairs which are overloaded.
-     */ 
+    /**
+     * The VSMCall function using this ParameterArray. ParameterArray needs this
+     * for interpreting return/reason code pairs which are overloaded.
+     */
     protected VSMCall function = null;
- 
+
     /**
      * Create an instance with no parameters.
-     */ 
+     */
     protected ParameterArray() {
         super();
     }
- 
+
     /**
      * Create an instance with {@code function} instanced.
+     *
      * @param function the function creating the parameter array so that this
-     *        information is available for return/reason code interpretation
-     */ 
+     * information is available for return/reason code interpretation
+     */
     public ParameterArray(VSMCall function) {
         super();
         this.function = function;
@@ -86,8 +86,9 @@ public class ParameterArray extends Vector <VSMParm> {
 
     /**
      * Replace the contents of this ParameterArray with the contents of another.
+     *
      * @param value the container of the replacement data
-     */ 
+     */
     public void setValue(ParameterArray value) {
         clear();
         if (value != null) {            // null is legal value
@@ -97,21 +98,22 @@ public class ParameterArray extends Vector <VSMParm> {
 
     /**
      * Get the function to an instance of which this is assigned.
+     *
      * @return function to an instance of which this is assigned
-     */ 
+     */
     public VSMCall getFunction() {
         return this.function;
-    } 
- 
+    }
+
     /**
      * Searches for a parameter with the specified formal name.
+     *
      * @param name formal name of parameter
      * @return parameter if found, else null.
-     */ 
+     */
     public VSMParm parameterNamed(String name) {
         VSMParm result = null;
-        for (Enumeration <VSMParm> e = elements(); e.hasMoreElements();) {
-            VSMParm v = e.nextElement();
+        for (VSMParm v : this) {
             if (v.getFormalName().equals(name)) {
                 result = v;
                 break;
@@ -122,37 +124,40 @@ public class ParameterArray extends Vector <VSMParm> {
 
     /**
      * Write the indexed parameter to the output stream.
+     *
      * @param index index in this ParameterArray to find the sought parameter at
      * @param out the output stream
      * @throws java.io.IOException on comm error
-     */ 
+     */
     public void writeParameterAt(int index, DataOutputStream out) throws IOException {
 
-        VSMParm p = elementAt(index);
+        VSMParm p = get(index);
         p.write(out);
     }
 
     /**
-     * Return a sum arrived at by recursive examination of members of the
-     * byte length of all the parameters combined (including the byte length
-     * of their transmitted counts. Note that if you have already run the
-     * composeInput() function, that function typically calls totalParameterLength()
-     * at the very end and prepends the total to the head of this ParameterArray.
-     * So if you call totalParameterLength() after the array is composed, it will
-     * already contain the length <tt>int4</tt> sent at the head of every packet so the
-     * new count from totalParameterLength() at that time will be 4 greater
-     * (length of the length <tt>int4</tt> now included) than sent as a length at transmission
-     * time.
+     * Return a sum arrived at by recursive examination of members of the byte
+     * length of all the parameters combined (including the byte length of their
+     * transmitted counts. Note that if you have already run the composeInput()
+     * function, that function typically calls totalParameterLength() at the
+     * very end and prepends the total to the head of this ParameterArray. So if
+     * you call totalParameterLength() after the array is composed, it will
+     * already contain the length <tt>int4</tt> sent at the head of every packet
+     * so the new count from totalParameterLength() at that time will be 4
+     * greater (length of the length <tt>int4</tt> now included) than sent as a
+     * length at transmission time.
      * <p>
      * In other words, there's no reason to call this at a high level except to
-     * manufacture the subsequently prepended length <tt>int4</tt> at composeInput().
+     * manufacture the subsequently prepended length <tt>int4</tt> at
+     * composeInput().
+     *
      * @return the sum arrived at by recursive examination of members of the
-     * byte length of all the parameters found at the time of examination in this
-     * ParameterArray.</p>
-     */ 
+     * byte length of all the parameters found at the time of examination in
+     * this ParameterArray.</p>
+     */
     public long totalParameterLength() {
         long total = 0;
-        Iterator <VSMParm> thisIterator = iterator();
+        Iterator<VSMParm> thisIterator = iterator();
         while (thisIterator.hasNext()) {
             total += thisIterator.next().paramLength();
         }
@@ -161,14 +166,17 @@ public class ParameterArray extends Vector <VSMParm> {
 
     /**
      * Write all parameters in order to the output stream.
+     *
      * @param out
      * @throws java.io.IOException
      * @see #readAll
-     * @see <a href="http://publib.boulder.ibm.com/infocenter/zvm/v5r3/topic/com.ibm.zvm.v53.dmse6/hcsl8b20.htm" target="top">z/VM V5R3.0 Systems Management Application Programming SC24-6122-03</a>
-     */ 
+     * @see
+     * <a href="http://publib.boulder.ibm.com/infocenter/zvm/v5r3/topic/com.ibm.zvm.v53.dmse6/hcsl8b20.htm" target="top">z/VM
+     * V5R3.0 Systems Management Application Programming SC24-6122-03</a>
+     */
     public void writeAll(DataOutputStream out) throws IOException {
-        /* Write all the params */ 
-        Iterator <VSMParm> myIterator = iterator();
+        /* Write all the params */
+        Iterator<VSMParm> myIterator = iterator();
         while (myIterator.hasNext()) {
             myIterator.next().write(out);
         }
@@ -176,79 +184,75 @@ public class ParameterArray extends Vector <VSMParm> {
     }
 
     /**
-     * Read in the overall message that is modeled by
-     * the compose() method of query/call classes.
+     * Read in the overall message that is modeled by the compose() method of
+     * query/call classes.
+     *
      * @param in
      * @throws java.io.IOException
      * @throws VSMException
      * @see #writeAll
-     * @see <a href="http://publib.boulder.ibm.com/infocenter/zvm/v5r3/topic/com.ibm.zvm.v53.dmse6/hcsl8b20.htm" target="top">z/VM V5R3.0 Systems Management Application Programming SC24-6122-03</a>
-     */ 
-    public void readAll(DataInputStream in) throws IOException,  VSMException {
-        /* Write all the params */ 
+     * @see
+     * <a href="http://publib.boulder.ibm.com/infocenter/zvm/v5r3/topic/com.ibm.zvm.v53.dmse6/hcsl8b20.htm" target="top">z/VM
+     * V5R3.0 Systems Management Application Programming SC24-6122-03</a>
+     */
+    public void readAll(DataInputStream in) throws IOException, VSMException {
+        /* Write all the params */
         int output_length = - 1;        // -1 means we haven't instanced it yet.
-        ListIterator <VSMParm> currentListIterator = listIterator();
+        ListIterator<VSMParm> currentListIterator = listIterator();
         ParameterArray replacement = new ParameterArray();
-        VSMParm previous = null;
+        VSMParm previous;
         while (currentListIterator.hasNext()) {
             VSMParm copyOfCurrentParm = currentListIterator.next().copyOf();
             // /* Debug */ System.err.println(" next list item in ParameterArray.readAll is:\n  " + copyOfCurrentParm);
             if (copyOfCurrentParm instanceof VSMInt) {
                 // /* Debug */ System.err.println(" reading a VSMInt ");
                 copyOfCurrentParm.read(in, - 1);
-                if (replacement.size() == 0) {                    // If this is the first thing we read
+                if (replacement.isEmpty()) {                    // If this is the first thing we read
                     replacement.add(copyOfCurrentParm);                    // ... it's the request_id_immediate
                     // ... and of course don't decrement output length
                     // ... since we haven't readoutput length yet.
-                } else {
-                    if (replacement.size() == 1) {                        // If this is the second thing we read
-                        // ... then this should be the remaining output length param.
-                        output_length = VSMInt4 .class.cast(copyOfCurrentParm).getValue();
-                        replacement.add(copyOfCurrentParm);                        // There's output_length!
-                    } else {                        // We're beyond the first two int4's and now into the output body
-                        replacement.add(copyOfCurrentParm);
-                        output_length -= copyOfCurrentParm.paramLength();
-                    }
+                } else if (replacement.size() == 1) {                        // If this is the second thing we read
+                    // ... then this should be the remaining output length param.
+                    output_length = VSMInt4.class.cast(copyOfCurrentParm).getValue();
+                    replacement.add(copyOfCurrentParm);                        // There's output_length!
+                } else {                        // We're beyond the first two int4's and now into the output body
+                    replacement.add(copyOfCurrentParm);
+                    output_length -= copyOfCurrentParm.paramLength();
                 }
 
                 // /* Debug */ System.err.println(" Value of read VSMInt " + copyOfCurrentParm.getFormalName() + " == " + VSMInt.class.cast(copyOfCurrentParm).getLongValue());
-            } else {
-                if (copyOfCurrentParm instanceof VSMString | copyOfCurrentParm instanceof VSMArray | copyOfCurrentParm instanceof VSMStruct) {
-                    if (! replacement.isEmpty()) {
-                        previous = replacement.lastElement();
-                        // /* Debug */ System.err.println("previous param is " + previous);
-                        if (previous instanceof VSMInt4) {
-                            int countLength = VSMInt4 .class.cast(previous).getValue();
-                            // /* Debug */ System.err.println(" Getting ready to read " + copyOfCurrentParm + " with read length " + countLength);
-                            // /* Debug */ System.err.flush();
-                            copyOfCurrentParm.read(in, countLength);
-                            output_length -= copyOfCurrentParm.paramLength();
-                            replacement.add(copyOfCurrentParm);
-                        } else {
-                            // The previous parm isn't the required count for the copyOfCurrentParm counted parmtype
-                            throw new ParameterArrayReadAllException(" Previous parm was not a count for the current counted parmtype. " + copyOfCurrentParm);
-                        }
-                    } else {
-                        // There's no count for the counted parmtype
-                        throw new ParameterArrayReadAllException(" There is no count for the current counted parmtype. " + copyOfCurrentParm);
-                    }
-                } else {
-                    if (copyOfCurrentParm instanceof VSMAsciiZ | copyOfCurrentParm instanceof VSMAsciiZArray) {
-                        copyOfCurrentParm.read(in, output_length);
+            } else if (copyOfCurrentParm instanceof VSMString | copyOfCurrentParm instanceof VSMArray | copyOfCurrentParm instanceof VSMStruct) {
+                if (!replacement.isEmpty()) {
+                    previous = replacement.get(replacement.size() - 1);
+                    // /* Debug */ System.err.println("previous param is " + previous);
+                    if (previous instanceof VSMInt4) {
+                        int countLength = VSMInt4.class.cast(previous).getValue();
+                        // /* Debug */ System.err.println(" Getting ready to read " + copyOfCurrentParm + " with read length " + countLength);
+                        // /* Debug */ System.err.flush();
+                        copyOfCurrentParm.read(in, countLength);
                         output_length -= copyOfCurrentParm.paramLength();
                         replacement.add(copyOfCurrentParm);
                     } else {
-                        throw new ParameterArrayReadAllException(" Unknown parameter type.");
+                        // The previous parm isn't the required count for the copyOfCurrentParm counted parmtype
+                        throw new ParameterArrayReadAllException(" Previous parm was not a count for the current counted parmtype. " + copyOfCurrentParm);
                     }
+                } else {
+                    // There's no count for the counted parmtype
+                    throw new ParameterArrayReadAllException(" There is no count for the current counted parmtype. " + copyOfCurrentParm);
                 }
+            } else if (copyOfCurrentParm instanceof VSMAsciiZ | copyOfCurrentParm instanceof VSMAsciiZArray) {
+                copyOfCurrentParm.read(in, output_length);
+                output_length -= copyOfCurrentParm.paramLength();
+                replacement.add(copyOfCurrentParm);
+            } else {
+                throw new ParameterArrayReadAllException(" Unknown parameter type.");
             }
 
             // System.err.flush();
             // System.err.println("how many times? " + howmanytimes++);
- 
-            /* Check that we don't read past end when we get error documents */ 
-            /* "- 2 * SIZEOF_INT4" because output_length doesn't count the */ 
-            /* immediate reply and the output_length itself */ 
+            /* Check that we don't read past end when we get error documents */
+ /* "- 2 * SIZEOF_INT4" because output_length doesn't count the */
+ /* immediate reply and the output_length itself */
             // if (output_length != -1 & output_length <= replacement.totalParameterLength() - 2 * SIZEOF_INT4) {
             if (output_length != - 1 & output_length <= 0) {
                 break;
@@ -262,10 +266,11 @@ public class ParameterArray extends Vector <VSMParm> {
 
     /**
      * Gen an interpretive string of params and fuc ret and reason codes.
+     *
      * @return an interpretive string of params and fuc ret and reason codes.
-     */ 
+     */
     public String prettyPrintAll() {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         sb.append(prettyPrintRCAndReason());
         sb.append(prettyPrintParams());
         return sb.toString();
@@ -273,11 +278,13 @@ public class ParameterArray extends Vector <VSMParm> {
 
     /**
      * Prettyprint to a string each param in the array.
-     * @return an interpretive string containing the prettyprint of each param in the array
-     */ 
+     *
+     * @return an interpretive string containing the prettyprint of each param
+     * in the array
+     */
     public String prettyPrintParams() {
-        StringBuffer sb = new StringBuffer();
-        Iterator <VSMParm> i = iterator();
+        StringBuilder sb = new StringBuilder();
+        Iterator<VSMParm> i = iterator();
         while (i.hasNext()) {
             sb.append(i.next().prettyPrint());
             sb.append("\n");
@@ -286,36 +293,43 @@ public class ParameterArray extends Vector <VSMParm> {
     }
 
     /**
-     * Examine the array for ret code and reason code and return an interpretive string.
+     * Examine the array for ret code and reason code and return an interpretive
+     * string.
+     *
      * @return an interpretive string for ret code and reason code
-     */ 
+     */
     public String prettyPrintRCAndReason() {
-        String result = null;
+        String result;
         VSMParm rc = parameterNamed("return_code");
         VSMParm reason = parameterNamed("reason_code");
         if (rc == null) {
             result = "No parameter named return_code found in " + this;
+        } else if (reason == null) {
+            result = "No parameter named reason_code found in " + this;
         } else {
-            if (reason == null) {
-                result = "No parameter named reason_code found in " + this;
-            } else {
-                VSMInt4 rc_int4 = VSMInt4 .class.cast(rc);
-                VSMInt4 reason_int4 = VSMInt4 .class.cast(reason);
-                result = VsmapiRC.prettyPrint(rc_int4.getValue(), reason_int4.getValue(), getFunction()) + "\n";
-            }
+            VSMInt4 rc_int4 = VSMInt4.class.cast(rc);
+            VSMInt4 reason_int4 = VSMInt4.class.cast(reason);
+            result = VsmapiRC.prettyPrint(rc_int4.getValue(), reason_int4.getValue(), getFunction()) + "\n";
         }
         return result;
     }
 
+    @Override
+    public String toString() {
+        return prettyPrintAll();
+    }
+
     /**
-     * Exception to throw if there's a Pigiron internal marshalling error in the read.
-     */ 
+     * Exception to throw if there's a Pigiron internal marshalling error in the
+     * read.
+     */
     public class ParameterArrayReadAllException extends VSMException {
 
         /**
          * Instance the exception with a specific message.
+         *
          * @param message the message
-         */ 
+         */
         public ParameterArrayReadAllException(String message) {
             super(message);
         }
