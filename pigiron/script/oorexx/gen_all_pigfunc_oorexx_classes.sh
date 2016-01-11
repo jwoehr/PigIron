1ac3d23e4141eb94eebe96ebdeb21ff1a000dbab
Jack J. Woehr <4604036+jwoehr@users.noreply.github.com>
2016-01-11 20:33:36 +0000

    autogen

---
+++ pigiron/script/oorexx/gen_all_pigfunc_oorexx_classes.sh
@@ -0,0 +1,66 @@
+# gen_all_pigfunc_oorexx_classes.sh
+# autogenerate all the oorexx classes representing PigIron functions
+#
+# Copyright (c) 2016, Jack J. Woehr jwoehr@softwoehr.com
+# PO Box 51, Golden, Colorado 80402-0051 USA
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#         notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#         notice, this list of conditions and the following disclaimer
+#         in the documentation and/or other materials provided with the
+#         distribution.
+#     * Neither the name of the PigIron Project nor the names of its
+#         contributors may be used to endorse or promote products derived
+#         from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+function usage {
+echo "$0 pigiron_root [dest_dir] [prefix]"
+echo " ... generates oorexx classes representing PigIron functions registered in pigfunc.cls"
+echo " ... classes are generated in dest_dir if present, otherwise, current directory"
+echo " ... pigiron_root is the directory above src/com/softwoehr ... etc"
+echo " ... prefix, if supplied, is an optional prefix to the class names."
+}
+
+pigroot=$1
+dest_dir=$2
+pigprefix=$3
+if [ "$pigroot" == "" ]
+then
+	usage
+	exit 1
+fi
+if [ "$dest_dir" != "" ]
+then
+	dest_dir="${dest_dir}/"
+fi
+curdir=`pwd`
+cd ${pigroot}/src/com/softwoehr/pigiron/functions
+for i in *.java
+do
+	classname=`basename $i .java`
+	if [ "$classname" != "package-info" ]
+	then
+		rexx ${curdir}/genPigFuncOORexxCls.rex $classname $pigprefix > ${dest_dir}${pigprefix}${classname}".cls"
+	fi
+done
+exit 0
+# end of file
